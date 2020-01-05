65c5f546bc48f5f4c0d74d058ccc695af13904f1
Jdery <ilove_matea@yahoo.com>
2020-01-06 01:25:48 +0200

    L1C# final with less bugs

--- Program.cs
+++ Program.cs
@@ -4,23 +4,36 @@ using System.Linq;
 using Lab1.Repository;
 using Lab1.UI;
 
-namespace Lab1
-{
-    internal class Program
-    {
-        private static List<string> _teamNames = new List<string>();
-        private static List<string> _schoolNames = new List<string>();
+namespace Lab1 {
+    internal class Program {
+        private static List<string> _teamNames = new List<string>(new[] {
+            "Houston Rockets", "Los Angeles Lakers", "LA Clippers", "Chicago Bulls", "Cleveland Cavaliers", "Utah Jazz",
+            "Brooklyn Nets", "New Orleans Pelicans", "Indiana Pacers", "Toronto Raptors", "Charlotte Hornets", "Phoenix Suns",
+            "Portland TrailBlazers", "Golden State Warriors", "Washington Wizards", "San Antonio Spurs", "Orlando Magic",
+            "Denver Nuggets", "Detroit Pistons", "Atlanta Hawks", "Dallas Mavericks", "Sacramento Kings", "Oklahoma City Thunder",
+            "Boston Celtics", "New York Knicks", "Minnesota Timberwolves", "Miami Heat", "Milwaukee Bucks"
+        });
 
-        public static void Main(string[] args)
-        {
-            string filePathDirectory = "./";
+        private static List<string> _schoolNames = new List<string>(new[] {
+            "Scoala Gimnaziala \"Octavian Goga\"", "Liceul Teoretic \"Lucian Blaga\"", "Scoala Gimnaziala \"Ioan Bob\"",
+            "Scoala Gimnaziala \"Ion Creanga\"", "Colegiul National Pedagogic \"Gheorghe Lazar\"",
+            "Scoala Gimnaziala Internationala SPECTRUM", "Colegiul National \"Emil Racovita\"", "Colegiul National \"George Cosbuc\"",
+            "Scoala Gimnaziala \"Ion Agarbiceanu\"", "Liceul Teoretic \"Avram Iancu\"", "Scoala Gimnaziala \"Constantin Brancusi\"",
+            "Liceul Teoretic \"Onisifor Ghibu\"", "Liceul cu Program Sportiv Cluj-Napoca", "Liceul Teoretic \"Nicolae Balcescu\"",
+            "Liceul Teoretic \"Gheorghe Sincai\"", "Scoala \"Nicolae Titulescu\"", "Scoala Gimnaziala \"Liviu Rebreanu\"",
+            "Scoala Gimnaziala \"Iuliu Hatieganu\"", "Liceul Teoretic \"Bathory Istvan\"", "Colegiul National \"George Baritiu\"",
+            "Liceul Teoretic \"Apaczai Csere Janos\"", "Seminarul Teologic Ortodox", "Liceul de Informatica \"Tiberiu Popoviciu\"",
+            "Scoala Gimnaziala \"Alexandru Vaida – Voevod\"", "Liceul Teoretic ELF", "Scoala Gimnaziala \"Gheorghe Sincai\" Floresti"
+        });
 
-            TeamFileRepository teamRepository =
-                new TeamFileRepository(new TeamValidator(), filePathDirectory + "teams.txt");
+        private static string filePathDirectory = "./";
+
+        public static void Main() {
+            TeamFileRepository teamRepository = new TeamFileRepository(new TeamValidator(), filePathDirectory + "teams.txt");
             Service<int, Team> teamService = new Service<int, Team>(teamRepository);
 
-            PlayerFileRepository playerRepository =
-                new PlayerFileRepository(new PlayerValidator(), filePathDirectory + "players.txt", teamRepository);
+            PlayerFileRepository playerRepository = new PlayerFileRepository(new PlayerValidator(),
+                filePathDirectory + "players.txt", teamRepository);
             Service<int, Player> playerService = new Service<int, Player>(playerRepository);
 
             GameFileRepository gameRepository =
@@ -31,9 +44,6 @@ namespace Lab1
                 new ActivePlayerFileRepository(new ActivePlayerValidator(), filePathDirectory + "activePlayers.txt");
             ActivePlayerService activePlayerService = new ActivePlayerService(activePlayersRepository);
 
-            AddTeamNames();
-            AddSchoolNames();
-
             //adds data to repositories
             AddJunk(teamRepository, playerRepository, gameRepository, activePlayersRepository);
 
@@ -43,40 +53,33 @@ namespace Lab1
         }
 
         private static void AddJunk(TeamFileRepository teamRepository, PlayerFileRepository playerRepository,
-            GameFileRepository gameRepository, ActivePlayerFileRepository activePlayersRepository)
-        {
+            GameFileRepository gameRepository, ActivePlayerFileRepository activePlayersRepository) {
             Random rand = new Random(DateTime.Now.Millisecond);
-            string[] firstNames =
-            {
-                "Jonathan", "Will A.", "Robert E. O.", "Dio", "George", "Joseph", "Caesar A.", "Lisa", "Rudol", "Suzi",
-                "Kars", "Esidisi", "Wamuu", "Santana", "Jotaro", "Muhammad", "Noriaki", "Jean Pierre", "Iggy", "Holy",
-                "Vanilla", "Hol", "Daniel J.", "Oingo", "Boingo", "Anubis", "Telence", "Josuke", "Okuyasu", "Koichi",
-                "Rohan", "Hayato", "Yukako", "Tonio", "Tomoko", "Yoshikage", "Giorno", "Bruno", "Leone", "Guido",
-                "Guido", "Narancia", "Trish", "Coco", "Pericolo", "Diavolo", "Cioccolata", "Polpo", "Pesci", ""
+            string[] firstNames = {
+                "Jonathan", "Will A.", "Robert E. O.", "Dio", "George", "Joseph", "Caesar A.", "Lisa", "Rudol", "Suzi", "Kars",
+                "Esidisi", "Wamuu", "Santana", "Jotaro", "Muhammad", "Noriaki", "Jean Pierre", "Iggy", "Holy", "Vanilla", "Hol",
+                "Daniel J.", "Oingo", "Boingo", "Anubis", "Telence", "Josuke", "Okuyasu", "Koichi", "Rohan", "Hayato", "Yukako",
+                "Tonio", "Tomoko", "Yoshikage", "Giorno", "Bruno", "Leone", "Guido", "Guido", "Narancia", "Trish", "Coco", "Pericolo",
+                "Diavolo", "Cioccolata", "Polpo", "Pesci", ""
             };
-            string[] lastNames =
-            {
-                "Joestar", "Zeppeli", "Speedwagon", "Brando", "Lisa", "von Stroheim", "Q", "Kujo", "Avdol", "Kakyoin",
-                "Polnareff", "Ice", "Horse", "D'Arby", "Higashikata", "Nijimura", "Hirose", "Kishibe", "Kawajiri",
-                "Yamagishi", "Trussardi", "Kira", "Giovanna", "Bucciarati", "Abbacchio", "Mista", "Ghirga", "Una",
-                "Jumbo", "Doppio", ""
+            string[] lastNames = {
+                "Joestar", "Zeppeli", "Speedwagon", "Brando", "Lisa", "von Stroheim", "Q", "Kujo", "Avdol", "Kakyoin", "Polnareff",
+                "Ice", "Horse", "D'Arby", "Higashikata", "Nijimura", "Hirose", "Kishibe", "Kawajiri", "Yamagishi", "Trussardi", "Kira",
+                "Giovanna", "Bucciarati", "Abbacchio", "Mista", "Ghirga", "Una", "Jumbo", "Doppio", ""
             };
 
             //adding random teams
             int id = 0;
-            foreach (string teamName in _teamNames)
-                teamRepository.Save(new Team(id++, teamName));
+            foreach (string teamName in _teamNames) teamRepository.Save(new Team(id++, teamName));
 
             //adding arandom players
             id = 0;
-            var schoolTeams = Enumerable.Range(0, _teamNames.Count).OrderBy(x => rand.Next()).Take(_schoolNames.Count)
-                .ToList();
-            foreach (var i in schoolTeams.Select((teamId, index) => (index, teamId)))
-            {
+            var schoolTeams = Enumerable.Range(0, _teamNames.Count).OrderBy(x => rand.Next()).Take(_schoolNames.Count).ToList();
+            foreach (var i in schoolTeams.Select((teamId, index) => (index, teamId))) {
                 Team team = teamRepository.FindOne(i.teamId);
                 for (int j = 0; j < rand.Next(15, 25); j++)
-                    playerRepository.Save(new Player(id++, GenerateRandomName(rand, firstNames, lastNames),
-                        _schoolNames[i.index], team));
+                    playerRepository.Save(new Player(id++, GenerateRandomName(rand, firstNames, lastNames), _schoolNames[i.index],
+                        team));
             }
 
             //adding random games
@@ -84,123 +87,45 @@ namespace Lab1
             DateTime end = DateTime.Today;
             int range = (end - start).Days;
             for (int i = 0; i < _schoolNames.Count - 1; i++)
-            for (int j = i + 1; j < _schoolNames.Count; j++)
-                if (rand.Next(3) == 0)
-                {
-                    Team team1 = teamRepository.FindOne(i);
-                    Team team2 = teamRepository.FindOne(j);
-                    if (schoolTeams.Contains(team1.Id) && schoolTeams.Contains(team2.Id))
-                        gameRepository.Save(new Game(team1, team2, start.AddDays(rand.Next(range))));
-                }
+                for (int j = i + 1; j < _schoolNames.Count; j++)
+                    if (rand.Next(3) == 0) {
+                        Team team1 = teamRepository.FindOne(i);
+                        Team team2 = teamRepository.FindOne(j);
+                        if (schoolTeams.Contains(team1.Id) && schoolTeams.Contains(team2.Id))
+                            gameRepository.Save(new Game(team1, team2, start.AddDays(rand.Next(range))));
+                    }
 
             //adding random active players
             var players = playerRepository.FindAll().ToList();
-            foreach (Game game in gameRepository.FindAll())
-            {
-                int team1Id = game.Id.Team1Id;
-                int team2Id = game.Id.Team2Id;
-
-                AddActivePlayerToGame(team1Id, team2Id, team1Id, players, rand, activePlayersRepository);
-                AddActivePlayerToGame(team1Id, team2Id, team2Id, players, rand, activePlayersRepository);
+            foreach (Game game in gameRepository.FindAll()) {
+                AddActivePlayerToGame(game.Id.Team1Id, game.Id.Team2Id, game.Id.Team1Id, players, rand, activePlayersRepository);
+                AddActivePlayerToGame(game.Id.Team1Id, game.Id.Team2Id, game.Id.Team2Id, players, rand, activePlayersRepository);
             }
         }
 
-        private static void AddActivePlayerToGame(int team1Id, int team2Id, int teamId, List<Player> players,
-            Random rand, ActivePlayerFileRepository activePlayersRepository)
-        {
+        private static void AddActivePlayerToGame(int team1Id, int team2Id, int teamId, List<Player> players, Random rand,
+            ActivePlayerFileRepository activePlayersRepository) {
             var teamPlayers = players.Where(p => p.Team.Id.Equals(teamId)).ToList();
             var teamPlaying = Enumerable.Range(0, teamPlayers.Count()).OrderBy(x => rand.Next()).ToArray();
             int i = 0;
-            foreach (Player player in teamPlayers)
-            {
-                if (teamPlaying[i] < 15)
-                {
+            foreach (Player player in teamPlayers) {
+                if (teamPlaying[i] < 15) {
                     int score = 0;
-                    if (teamPlaying[i] < 11)
-                    {
+                    if (teamPlaying[i] < 11) {
                         int scoreProbability = rand.Next(1, 101);
                         if (scoreProbability > 95)
                             score = 2;
-                        else if (scoreProbability > 80)
-                            score = 1;
-                        activePlayersRepository.Save(new ActivePlayer(player.Id, team1Id, team2Id, score,
-                            ActivityState.Playing));
-                    }
-                    else
-                        activePlayersRepository.Save(new ActivePlayer(player.Id, team1Id, team2Id, score,
-                            ActivityState.Reserve));
+                        else if (scoreProbability > 80) score = 1;
+                        activePlayersRepository.Save(new ActivePlayer(player.Id, team1Id, team2Id, score, ActivityState.Playing));
+                    } else
+                        activePlayersRepository.Save(new ActivePlayer(player.Id, team1Id, team2Id, score, ActivityState.Reserve));
                 }
 
                 i++;
             }
         }
 
-
-        private static void AddTeamNames()
-        {
-            _teamNames.Add("Houston Rockets");
-            _teamNames.Add("Los Angeles Lakers");
-            _teamNames.Add("LA Clippers");
-            _teamNames.Add("Chicago Bulls");
-            _teamNames.Add("Cleveland Cavaliers");
-            _teamNames.Add("Utah Jazz");
-            _teamNames.Add("Brooklyn Nets");
-            _teamNames.Add("New Orleans Pelicans");
-            _teamNames.Add("Indiana Pacers");
-            _teamNames.Add("Toronto Raptors");
-            _teamNames.Add("Charlotte Hornets");
-            _teamNames.Add("Phoenix Suns");
-            _teamNames.Add("Portland TrailBlazers");
-            _teamNames.Add("Golden State Warriors");
-            _teamNames.Add("Washington Wizards");
-            _teamNames.Add("San Antonio Spurs");
-            _teamNames.Add("Orlando Magic");
-            _teamNames.Add("Denver Nuggets");
-            _teamNames.Add("Detroit Pistons");
-            _teamNames.Add("Atlanta Hawks");
-            _teamNames.Add("Dallas Mavericks");
-            _teamNames.Add("Sacramento Kings");
-            _teamNames.Add("Oklahoma City Thunder");
-            _teamNames.Add("Boston Celtics");
-            _teamNames.Add("New York Knicks");
-            _teamNames.Add("Minnesota Timberwolves");
-            _teamNames.Add("Miami Heat");
-            _teamNames.Add("Milwaukee Bucks");
-        }
-
-        private static void AddSchoolNames()
-        {
-            _schoolNames.Add("Scoala Gimnaziala \"Horea\"");
-            _schoolNames.Add("Scoala Gimnaziala \"Octavian Goga\"");
-            _schoolNames.Add("Liceul Teoretic \"Lucian Blaga\"");
-            _schoolNames.Add("Scoala Gimnaziala \"Ioan Bob\" ");
-            _schoolNames.Add("Scoala Gimnaziala \"Ion Creanga\"");
-            _schoolNames.Add("Colegiul National Pedagogic \"Gheorghe Lazar\" ");
-            _schoolNames.Add("Scoala Gimnaziala Internationala SPECTRUM");
-            _schoolNames.Add("Colegiul National \"Emil Racovita\"");
-            _schoolNames.Add("Colegiul National \"George Cosbuc\"");
-            _schoolNames.Add("Scoala Gimnaziala \"Ion Agarbiceanu\"");
-            _schoolNames.Add("Liceul Teoretic \"Avram Iancu\"");
-            _schoolNames.Add("Scoala Gimnaziala \"Constantin Brancusi\"");
-            _schoolNames.Add("Liceul Teoretic \"Onisifor Ghibu\"");
-            _schoolNames.Add("Liceul cu Program Sportiv Cluj-Napoca");
-            _schoolNames.Add("Liceul Teoretic \"Nicolae Balcescu\"");
-            _schoolNames.Add("Liceul Teoretic \"Gheorghe Sincai\"");
-            _schoolNames.Add("Scoala \"Nicolae Titulescu\"");
-            _schoolNames.Add("Scoala Gimnaziala \"Liviu Rebreanu\"");
-            _schoolNames.Add("Scoala Gimnaziala \"Iuliu Hatieganu\"");
-            _schoolNames.Add("Liceul Teoretic \"Bathory Istvan\"");
-            _schoolNames.Add("Colegiul National \"George Baritiu\"");
-            _schoolNames.Add("Liceul Teoretic \"Apaczai Csere Janos\"");
-            _schoolNames.Add("Seminarul Teologic Ortodox");
-            _schoolNames.Add("Liceul de Informatica \"Tiberiu Popoviciu\"");
-            _schoolNames.Add("Scoala Gimnaziala \"Alexandru Vaida – Voevod\"");
-            _schoolNames.Add("Liceul Teoretic ELF");
-            //_schoolNames.Add("Scoala Gimnaziala \"Gheorghe Sincai\" Floresti");
-        }
-
-        private static string GenerateRandomName(Random rand, string[] firstName, string[] lastName)
-        {
+        private static string GenerateRandomName(Random rand, string[] firstName, string[] lastName) {
             return (firstName[rand.Next(firstName.Length)] + " " + lastName[rand.Next(lastName.Length)]).Trim();
         }
     }
